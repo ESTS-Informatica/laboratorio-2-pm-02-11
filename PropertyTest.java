c0b8e99d2967f1661b679f27ff91e0b024a2185c
martallopess <202300192@estudantes.ips.pt>
2024-03-20 11:54:18 +0000

    fssfsd

--- PropertyTest.java
+++ PropertyTest.java
@@ -1,19 +1,18 @@
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class PropertyTest 
 {
 
-    @Before
+    @Test
     public void setUp() {
-        //Property property1 = new Property("T3 Monte Belo", 150000.0); // Fixture
+        Property property1 = new Property("T3 Monte Belo", 150000.0); // Fixture
     }
 
     @Test
     public void testConstructor() {
         Property property1 = new Property("T3 Monte Belo", 15.0); // Fixture
-       // assertEquals("T3 Monte Belo", property1.getDescription());
+        assertEquals("T3 Monte Belo", property1.getDescription());
         assertEquals(15.0, property1.getPrice());
     }
 
